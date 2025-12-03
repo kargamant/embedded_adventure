c27716bc34ac6485b0af453a013c5dce6c02ed5a
kargamant <firexholms@gmail.com>
2025-12-03 11:16:38 +0300

    wifi scan done

--- wifi_experiments/main/wifi_experiments.c
+++ wifi_experiments/main/wifi_experiments.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "esp_wifi.h"
+#include "nvs_flash.h"
 
 void app_main(void)
 {
     printf("Hello, monkeys!\n\n");
 
-    // first deinit
-    esp_wifi_stop();
-    printf("wifi service stopped!\n");
+    nvs_flash_init();
 
-    esp_wifi_deinit();
-    printf("wifi driver deinitted!\n");
-
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    printf("some default things\n");
 
     wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&config);
@@ -27,17 +27,33 @@ void app_main(void)
     printf("wifi scan started!\n");
 
     // retrievement
-    uint16_t ap_number = 3;
-    wifi_ap_record_t ap_records[3];
+    uint16_t ap_number = 10;
+    wifi_ap_record_t ap_records[10];
     esp_wifi_scan_get_ap_records(&ap_number, ap_records);
 
-    for(int i=0; i<3; i++)
+    for(int i=0; i<ap_number; i++)
     {
         wifi_ap_record_t ap = ap_records[i];
         printf("wifi_name: %s\n", (const char*)ap.ssid);
-        printf("mac address: %d\n", *ap.bssid);
+        printf("mac address: %x:%x:%x:%x:%x:%x\n", ap.bssid[0], ap.bssid[1], ap.bssid[2], ap.bssid[3], ap.bssid[4], ap.bssid[5]);
     }
 
+    // connect
+
+    // wifi_interface_t mode = WIFI_IF_STA;
+    // unsigned char network_name[32] = "Sunshine";
+    // unsigned char password[64] = "140009Pobratimov2275"; 
+    // wifi_sta_config_t configuration;
+    // configuration.ssid = (uint8_t[32]){'S', 'u', 'n', 's', 'h', 'i', 'n', 'e'};
+    // configuration.password = password;
+    // esp_wifi_set_config(mode, &configuration);
+    
+    // esp_err_t err_code = esp_wifi_connect();
+
+    // if(err_code == ESP_OK)
+    //     printf("successssssssssssssssssss\n");
+    // else
+    //     printf("couldn't connect sorry bro\n");
 
 
     // stopping point
