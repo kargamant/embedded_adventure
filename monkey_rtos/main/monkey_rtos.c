d2c36c465301f4b1d9a9538b61b53d7e886dbb9c
kargamant <firexholms@gmail.com>
2025-12-22 18:10:05 +0300

    freertos task creation fix

--- monkey_rtos/main/monkey_rtos.c
+++ monkey_rtos/main/monkey_rtos.c
@@ -14,11 +14,16 @@ void monkey_task(void* params)
 void app_main(void)
 {
     printf("Hello, monkeys!\n\n");
-    char* params = "embedded";
+    static char params[8] = "embedded";
     TaskHandle_t task_handle = NULL;
 
     // rtos create task
-    xTaskCreate(monkey_task, "monkey_name", 1, params, 2, &task_handle);
+    xTaskCreate(monkey_task, "monkey_name", 1024, params, tskIDLE_PRIORITY, &task_handle);
+    printf("task was succesfully!\n\n");
+
+    params[0] = 'g';
+
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
 
     if(task_handle)
     {
