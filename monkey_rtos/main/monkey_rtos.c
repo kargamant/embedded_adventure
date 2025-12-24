ceed997f624c6ded0ef1edcf094266e7a05d6974
kargamant <firexholms@gmail.com>
2025-12-24 11:46:03 +0300

    second task creation

--- monkey_rtos/main/monkey_rtos.c
+++ monkey_rtos/main/monkey_rtos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
+#include "sdkconfig.h"
 #include "freertos/task.h"
 
 void monkey_task(void* params)
@@ -7,7 +8,16 @@ void monkey_task(void* params)
     while(1)
     {
         printf("monkey_name: %s\n", (char*)params);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        vTaskDelay(4000 / portTICK_PERIOD_MS);
+    }
+}
+
+void donkey_task(void* params)
+{
+    while(1)
+    {
+        printf("donkey_name: %s\n", (char*)params);
+        vTaskDelay(4000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -15,18 +25,25 @@ void app_main(void)
 {
     printf("Hello, monkeys!\n\n");
     static char params[8] = "embedded";
-    TaskHandle_t task_handle = NULL;
+    TaskHandle_t m_task_handle = NULL;
+    TaskHandle_t d_task_handle = NULL;
 
     // rtos create task
-    xTaskCreate(monkey_task, "monkey_name", 1024, params, tskIDLE_PRIORITY, &task_handle);
-    printf("task was succesfully!\n\n");
+    xTaskCreate(monkey_task, "monkey_name", 1024, params, tskIDLE_PRIORITY + 1, &m_task_handle);
+    xTaskCreate(donkey_task, "donkey_name", 1024, params, tskIDLE_PRIORITY + 1, &d_task_handle);
+    printf("tasks was succesfully created!\n\n");
 
     params[0] = 'g';
 
     vTaskDelay(3000 / portTICK_PERIOD_MS);
 
-    if(task_handle)
+    if(m_task_handle)
     {
-        vTaskDelete(task_handle);
+        vTaskDelete(m_task_handle);
+    }
+
+    if(d_task_handle)
+    {
+        vTaskDelete(d_task_handle);
     }
 }
